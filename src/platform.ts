d9d04248f4f410341d932ffaeab52c7a97032c34
Rick Bargerhuff <agilisone@gmail.com>
2025-03-25 22:19:54 -0400

    1.1.2: Code changes and optimizations. Polling added.
      - Polling has been added as a backup to catch state mismatches or missed push notifications. The polling interval can be specified for each controller added.
    
      - Code has been cleaned up and changes have been made to allow the code to be more efficient.
    
      - Validation added to verify the plugin's configuration.

--- src/platform.ts
+++ src/platform.ts
@@ -33,6 +33,17 @@ export class SmartRecirc32Platform implements DynamicPlatformPlugin {
     // to start discovery of new accessories.
     this.api.on('didFinishLaunching', () => {
       log.debug('Executed didFinishLaunching callback');
+
+      // Validate plugin's configuration.
+      log.info('Validating plugin\'s configuration...');
+      const isValid = this.validateConfiguration();
+      
+      if (!isValid) {
+        log.error('Cannot continue with device registration.');
+
+        return;
+      }
+
       // Register platform devices as accessories.
       this.registerDevices();
     });
@@ -43,7 +54,7 @@ export class SmartRecirc32Platform implements DynamicPlatformPlugin {
    * It should be used to set up event handlers for characteristics and update respective values.
    */
   configureAccessory(accessory: PlatformAccessory) {
-    this.log.info('Loading accessory from cache:', accessory.displayName);
+    this.log.info('Loading accessory from cache:', accessory.displayName);    
 
     // Add the restored accessory to the accessories cache, so we can track if it has already been registered    
     this.accessories.set(accessory.UUID, accessory);
@@ -57,7 +68,7 @@ export class SmartRecirc32Platform implements DynamicPlatformPlugin {
   registerDevices() {
     // There is no discovery taking place. Instead use the user-defined array in the platform config.
     const platformDevices = this.config.devices;
-    
+
     // Iterate over the discovered devices and register each one if it has not already been registered.
     for (const device of platformDevices) {
       // Generate a unique id for the accessory.
@@ -66,10 +77,31 @@ export class SmartRecirc32Platform implements DynamicPlatformPlugin {
       // Check to see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above.
       const existingAccessory = this.accessories.get(uuid);
+      const minPollingSeconds = 5;
+      const maxPollingSeconds = 300;
 
       if (existingAccessory) {
         // the accessory already exists
         this.log.info('Restoring existing accessory from cache:', existingAccessory.displayName);
+
+        const requestedPolling = device.pollingInterval ?? 30; // Fallback default.
+        const clampedPolling = Math.min(Math.max(requestedPolling, minPollingSeconds), maxPollingSeconds);
+        const accessoryName = device.name;
+
+        if (requestedPolling !== clampedPolling) {
+          this.log.warn(
+            `${accessoryName} : Polling interval of ${requestedPolling} is out of range. Allowed range is ${minPollingSeconds}-${maxPollingSeconds} seconds.`);
+          
+          this.log.warn(`${accessoryName} : Polling interval clamped to ${clampedPolling} seconds.`);
+        }
+
+        existingAccessory.context.device = {
+          ...device,
+          pollingInterval: clampedPolling,
+        };
+
+        // Update the platform accessory in case changes were made to the accessory's context.
+        this.api.updatePlatformAccessories([existingAccessory]);
 
         // Create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`.
@@ -84,7 +116,22 @@ export class SmartRecirc32Platform implements DynamicPlatformPlugin {
 
         // Store a copy of the device object in the `accessory.context`.
         // The `context` property can be used to store any data about the accessory you may need.
-        accessory.context.device = device;
+
+        const requestedPolling = device.pollingInterval ?? 30; // Fallback default.
+        const clampedPolling = Math.min(Math.max(requestedPolling, minPollingSeconds), maxPollingSeconds);
+        const accessoryName = device.name;
+
+        if (requestedPolling !== clampedPolling) {
+          this.log.warn(
+            `${accessoryName} : Polling interval of ${requestedPolling} is out of range. Allowed range is ${minPollingSeconds}-${maxPollingSeconds} seconds.`);
+          
+          this.log.warn(`${accessoryName} : Polling interval clamped to ${clampedPolling} seconds.`);
+        }
+
+        accessory.context.device = {
+          ...device,
+          pollingInterval: clampedPolling,
+        };
 
         // Create the accessory handler for the newly created accessory
         // Imported from `platformAccessory.ts`.
@@ -96,6 +143,68 @@ export class SmartRecirc32Platform implements DynamicPlatformPlugin {
 
       // Push into discoveredCacheUUIDs
       this.discoveredCacheUUIDs.push(uuid);
+    }
+  }
+
+  /**
+   * Validates that the plugin's configuration.
+   * 
+   * @returns boolean
+   */
+  validateConfiguration():boolean {
+    const devicesToValidate = this.config.devices;
+    const nameMap: { [key: string]: boolean } = {};
+    const uidMap: { [key: string]: boolean } = {};
+    const controllerNameMap: { [key: string]: boolean } = {};
+    const portMap: { [key: number]: boolean } = {};
+    const errors: string[] = [];
+
+    // Verify that there is at least one device to configure.
+    if (!devicesToValidate || !Array.isArray(devicesToValidate) || !devicesToValidate.length) {
+      this.log.error('No devices found in configuration. Please verify that at least one device has been added to the plugin\'s configuration.');
+
+      return false;
+    }
+
+    // Iterate through each device that has been added to the configuration.
+    for (const device of devicesToValidate) {
+      // Validate Name.
+      if (device.name in nameMap) {
+        errors.push(`Duplicate "Name" detected: "${device.name}". Each device name must be unique.`);
+      } else {
+        nameMap[device.name] = true;
+      }
+
+      // Validate Unique UID.
+      if (device.deviceUid in uidMap) {
+        errors.push(`Duplicate "Unique ID" detected: "${device.deviceUid}". The ID for each device must be unique.`);
+      } else {
+        uidMap[device.deviceUid] = true;
+      }
+
+      // Validate Controller Name.
+      if (device.controllerName in controllerNameMap) {
+        errors.push(`Duplicate "Controller Name" detected: "${device.controllerName}". The controller name for each device must be unique.`);
+      } else {
+        controllerNameMap[device.controllerName] = true;
+      }
+
+      // Validate for Port.
+      if (device.serverConfig.port in portMap) {
+        errors.push(`Duplicate "Port" detected: ${device.serverConfig.port}. The port for each device's server configuration must be unique.`);
+      } else {
+        portMap[device.serverConfig.port] = true;
+      }
+    }
+
+    // Throw one big error if anything failed.
+    if (errors.length > 0) {
+      const errorMsg = 'Configuration validation failed:\n' + errors.map(e => ` - ${e}`).join('\n');
+      this.log.error(errorMsg);
+
+      return false;
+    } else {
+      return true;
     }
   }
 }
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,4 +1,4 @@
-import type { PlatformAccessory, Service } from 'homebridge';
+import type { CharacteristicValue, PlatformAccessory, Service } from 'homebridge';
 import type { SmartRecirc32Platform } from './platform.js';
 import * as http from 'http';
 
@@ -8,7 +8,7 @@ import * as http from 'http';
  */
 export class SmartRecirc32PlatformAccessory {
   private service: Service;
-  private pumpOnStatus = false;
+  private controllerOnStatus = false;
 
   constructor(
     private readonly platform: SmartRecirc32Platform,
@@ -30,119 +30,169 @@ export class SmartRecirc32PlatformAccessory {
 
     // Register handlers for the On/Off Characteristic.
     this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this)); // SET - bind to the `setOn` method below
+      .onSet(this.setControllerOn.bind(this)); // SET - bind to the `setControllerOn` method below.
 
-    // Create an Http server so that we can listen for requests from our accessory.
+    // Create an HTTP server so that we can listen for requests from our accessory.
     const accessoryName = this.accessory.context.device.name;
     const serverConfig = this.accessory.context.device.serverConfig;
-    const server = http.createServer((req: http.IncomingMessage, resp: http.ServerResponse) => {
-      // Check if the URL matches your desired endpoint
-      let reqUrl = req.url;
-      let path = (serverConfig.path as string).toLowerCase();
-      path = this.prepPath(path);
+    const server = http.createServer(this.handleServerRequest.bind(this));
+    const port = serverConfig.port;
+    
+    server.listen(port, () => {
+      this.platform.log.info(`${accessoryName} : HTTP server listening on port ${port}`);
+    });
+
+    this.checkControllerStatus();
+
+    // Set a timer to execute the checkStatus() function every minute.
+    const pollingSeconds = this.accessory.context.device.pollingInterval as number;
+    const pollingInterval = (pollingSeconds * 1000);
+    this.platform.log.info(`${accessoryName} : Polling status every ${pollingSeconds} seconds`);
+    setInterval(this.checkControllerStatus.bind(this), pollingInterval);
+  }
+
+  /**
+   * Retrieves the controller's status so that we can stay in sync.
+   */
+  async checkControllerStatus() {    
+    const accessoryName = this.accessory.context.device.name;
+    const specificPath = '/status/';
+    const url = this.returnCommandUrl() + specificPath;
+
+    try {
+      const fetchResp = await fetch(url);
+
+      if (!fetchResp.ok) {
+        this.platform.log.error(`${accessoryName} : Could not retrieve the controller's status. Failed response from ${url} ->`, fetchResp.status);
+      }
       
-      if (reqUrl !== undefined) {
-        reqUrl = reqUrl.toLowerCase();
+      // Update the accessory's new status.
+      const responseData = await fetchResp.text();
+      this.platform.log.debug(`${accessoryName} : Controller responded with the following status: ->`, responseData);
+      const controllerOnStatus = (Number(responseData.trim()) === 1);
 
-        if (reqUrl.indexOf(path) >= 0) {
-          this.platform.log.debug(accessoryName + ': Received external HTTP request. Request URL ->', req.url);
-          
-          // Determine the status of the accessory.
-          const pumpStatus = reqUrl.split(path)[1];
-          
-          // Check if the pump status received is valid.
-          switch (pumpStatus) {
-          case 'pump_on':            
-          case 'pump_off':
-            break;
-          default:
-            resp.statusCode = 204;
-            resp.setHeader('Content-Length', 0);
-            resp.end();
+      // Update Homekit with the new status.
+      this.service.updateCharacteristic(this.platform.Characteristic.On, controllerOnStatus);
+      this.platform.log.debug(`${accessoryName} : Controller's status: ->`, controllerOnStatus);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        this.platform.log.error(`${accessoryName} : Unknown error has occurred while attempting to retrieve the controller's status: ${error.message}`);
+      }
+    }   
+  }
 
-            this.platform.log.error(accessoryName + ': Invalid pump status \'' + pumpStatus + '\' received. Request URL ->', req.url);
+  /**
+   * Handles requests to the HTTP server.
+   * 
+   * @param req 
+   * @param resp
+   * @returns 
+   */
+  handleServerRequest(req: http.IncomingMessage, resp: http.ServerResponse) {
+    const accessoryName = this.accessory.context.device.name;
+    const serverConfig = this.accessory.context.device.serverConfig;
+    let controllerOnStatus = false;
+    
+    // Check if the URL matches the desired endpoint.
+    let reqUrl = req.url;
+    let path = (serverConfig.path as string).toLowerCase();
+    path = this.prepPath(path);
+    
+    if (reqUrl !== undefined) {
+      reqUrl = reqUrl.toLowerCase();
 
-            return;
-          }
-
-          // Prevent the accessory's 'ON' characteristic from being set twice.
-          // This occurs when user triggers the accessory manually and the accessory then sends an HTTP request notifying the HTTP server
-          // of its status.
-          if ((pumpStatus === 'pump_on' && this.pumpOnStatus) || (pumpStatus === 'pump_off' && !this.pumpOnStatus)) {
-            this.platform.log.debug(accessoryName + ': Ignoring HTTP request. Status is already set to "' + pumpStatus + '."');
-
-            resp.statusCode = 200;
-            resp.end('OK');
-
-            return;
-          }
-
-          switch (pumpStatus) {
-          case 'pump_on':
-            this.pumpOnStatus = true;
-            
-            break;
-          case 'pump_off':
-            this.pumpOnStatus = false;
-            
-            break;
-          }
-
-          // Update Homekit with the new status.
-          this.service.updateCharacteristic(this.platform.Characteristic.On, this.pumpOnStatus);
-          
-          resp.statusCode = 200;
-          resp.end('OK');
-
-          this.platform.log.info(accessoryName + ': Set Characteristic On ->', this.pumpOnStatus);
-          this.platform.log.debug(accessoryName + ': HTTP request successfully processed.');
-        } else {
+      if (reqUrl.indexOf(path) >= 0) {
+        this.platform.log.debug(`${accessoryName} : HTTP server received external request. Request URL ->`, req.url);
+        
+        // Determine the status of the accessory.
+        const pumpStatus = reqUrl.split(path)[1];
+        
+        // Check if the pump status received is valid.
+        switch (pumpStatus) {
+        case 'pump_on':            
+        case 'pump_off':
+          break;
+        default:
           resp.statusCode = 204;
           resp.setHeader('Content-Length', 0);
           resp.end();
 
-          if (reqUrl.indexOf('favicon') === -1) {
-            this.platform.log.info(accessoryName + ': External request received and ignored. Request URL ->', req.url);            
-          }
+          this.platform.log.error(`${accessoryName} : Invalid pump status "${pumpStatus}" received. Request URL ->`, req.url);
+
+          return;
+        }
+
+        switch (pumpStatus) {
+        case 'pump_on':
+          controllerOnStatus = true;
+          break;
+        case 'pump_off':
+          controllerOnStatus = false;
+          break;
+        }
+
+        // Update Homekit with the new status.
+        this.service.updateCharacteristic(this.platform.Characteristic.On, controllerOnStatus);
+        
+        resp.statusCode = 200;
+        resp.end('OK');
+
+        this.platform.log.info(`${accessoryName} : Set Characteristic On ->`, controllerOnStatus);
+        this.platform.log.debug(`${accessoryName} : HTTP request successfully processed.`);
+      } else {
+        resp.statusCode = 204;
+        resp.setHeader('Content-Length', 0);
+        resp.end();
+
+        if (reqUrl.indexOf('favicon') === -1) {
+          this.platform.log.info(`${accessoryName} : External request received and ignored. Request URL ->`, req.url);
         }
       }
-    });
+    }
+  }
+
+  /**
+   * Returns the URL to use when issuing a command to the controller or retrieving its status.
+   * 
+   * @returns string
+   */
+  returnCommandUrl():string {
+    const controllerName = (this.accessory.context.device.controllerName as string).toLowerCase();
+    const specificName = 'ld-' + controllerName + '.local';
+    const specificPort = '3030';
     
-    const port = serverConfig.port;
-    server.listen(port, () => {
-      this.platform.log.info(accessoryName + ': HTTP server for accessory listening on port ' + port);
-    });
+    return `http://${specificName}:${specificPort}`;
   }
 
   /**
    * Handle "SET" requests from HomeKit
+   * 
+   * Issuing the HTTP web request will cause the controller to turn on or off. This in turn will cause the controller to return
+   * the controller's status to the Homebridge plugin.
    */
-  async setOn() {
-    const pumpOn = !this.pumpOnStatus;
-    const currentValue = new Number(pumpOn);    
+  async setControllerOn(value: CharacteristicValue) {
+    // Sync the status by fetching it from the controller.
+    const targetState = value as boolean;
     const accessoryName = this.accessory.context.device.name;
-    const controllerName = (this.accessory.context.device.controllerName as string).toLowerCase();
-    const specificName = 'ld-' + controllerName + '.local';
-    const specificPort = '3030';
-    const specificPath = '/pump/';    
-    const url = 'http://' + specificName  + ':' + specificPort + specificPath + currentValue.toString();
-    
-    this.platform.log.debug(accessoryName + ': Sending HTTP request to -> ' + url);
+    const specificPath = '/pump/';
+    const url = this.returnCommandUrl() + specificPath + (targetState ? '1' : '0');
+   
+    this.platform.log.info(`${accessoryName} : Received request from HomeKit to turn ${(targetState ? 'ON' : 'OFF')}`);
+    this.platform.log.debug(`${accessoryName} : Sending HTTP request to ->'`, url);
 
     try {
-      // Use fetch() to make http request that will toggle the controller.
+      // Use fetch() to make http request that will toggle the controller's status.
       const fetchResp = await fetch(url);
 
       if (!fetchResp.ok) {
-        this.platform.log.error(accessoryName + ': Unable to send HTTP request to -> ' + url, fetchResp.status);
+        this.platform.log.error(`${accessoryName} : Command sent to controller was not successful. URL: ${url}`, fetchResp.status);
       }
       
-      // Update Homekit with the new status.
-      this.pumpOnStatus = pumpOn;
-      this.platform.log.info(accessoryName + ': Set Characteristic On ->', this.pumpOnStatus);
+      // Update HomeKit (it will get corrected by webhook or polling if needed).
+      this.service.updateCharacteristic(this.platform.Characteristic.On, targetState);
     } catch (error: unknown) {      
       if (error instanceof Error) {
-        this.platform.log.error(accessoryName + ': Unable to send HTTP request: ' + error.message);
+        this.platform.log.error(`${accessoryName} : Unknown error has occurred while attempting to send command to the controller: ${error.message}`);
       }      
     }
   }
